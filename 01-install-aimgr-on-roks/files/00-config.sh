2ad06a4a5dcb4e29cb5db5bb18a930e7ad0012f9
sreenushankar <76681804+sreenushankar@users.noreply.github.com>
2023-03-15 14:58:39 +0530

    Update 00-config.sh

--- 01-install-aimgr-on-roks/files/00-config.sh
+++ 01-install-aimgr-on-roks/files/00-config.sh
@@ -7,4 +7,4 @@ export SUBSCRIPTION_CHANNEL=v3.6.2
 export NAMESPACE=cp4waiops
 
 export AI_MANAGER_STORAGE_CLASS=ibmc-file-gold-gid
-export AI_MANAGER_STORAGE_CLASS_LARGE_BLOCK=ibmc-file-gold-gid
+export AI_MANAGER_STORAGE_CLASS_LARGE_BLOCK=ibmc-block-gold
